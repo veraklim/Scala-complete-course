0216730f8e865a94dd483e15c26edb00785309ef
Vera Klimina <veraklim>
2018-11-06 12:22:43 +0300

    task4 chg1 merge

--- src/main/scala/lectures/collections/MergeSortImpl.scala
+++ src/main/scala/lectures/collections/MergeSortImpl.scala
@@ -7,12 +7,12 @@ package lectures.collections
   */
 object MergeSortImpl extends App {
 
-  private def merge(data1: Seq[Int], data2: Seq[Int]): Seq[Int] = (data1, data2) match {
-    case (Nil, data2) => data2
-    case (data1, Nil) => data1
+  private def merge(data1: Seq[Int], data2: Seq[Int], acc: Seq[Int]): Seq[Int] = (data1, data2) match {
+    case (Nil, data2) => acc ++ data2
+    case (data1, Nil) => acc ++ data1
     case _ => {
-        if (data1.head <= data2.head) data1.head +: merge(data1.tail, data2)
-        else data2.head +: merge(data1, data2.tail)
+        if (data1.head <= data2.head) merge(data1.tail, data2, acc :+ data1.head)
+        else merge(data1, data2.tail, acc :+ data2.head)
       }
   }
 
@@ -20,9 +20,10 @@ object MergeSortImpl extends App {
     val len = data.length
     if (len <= 1) data
     else {
-      merge(mergeSort(data.slice(0, len / 2)), mergeSort(data.slice(len / 2, len)))
+      merge(mergeSort(data.slice(0, len / 2)), mergeSort(data.slice(len / 2, len)), Seq())
     }
   }
 
- require(mergeSort(Seq(4, 8, 5, 1, 6, 0)) == List(0, 1, 4, 5, 6, 8))
+  require(mergeSort(Seq(4, 8, 5, 1, 6, 0)) == List(0, 1, 4, 5, 6, 8))
+  //println(mergeSort(Seq(4, 8, 5, 1, 6, 0)))
 }
